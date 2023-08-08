fd7d707776d6299cb29875d40dc9dbf693ddd36e
Sabrina C. Sales <sacsales@icloud.com>
2023-08-08 20:54:26 +0400

    Initial Commit

---
+++ FriendsAppFixed/FriendsAppFixedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppFixedApp.swift
+//  FriendsAppFixed
+//
+//  Created by Sabrina C. Sales on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppFixedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
